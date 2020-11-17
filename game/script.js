21b5db533df5f905ff920ee0666412250568562b
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2020-11-17 14:32:31 +0100

    Classwork1 Game from Mariia Morenko

---
+++ game/script.js
@@ -0,0 +1,71 @@
+function GameObject(imageUrl){
+    this.x=0;
+    this.y=0;
+    this.ready=false;
+    this.image=new Image();
+    this.image.onload=()=>this.ready=true; // нужна стрелочная функция, чтобы переопределить this к event
+    if (imageUrl){
+        this.image.src=imageUrl;
+    }
+}
+GameObject.prototype.render=function (ctx){//обобщающая функция для загрузки картинки, ctx-контекст
+    if (this.ready) {
+        ctx.drawImage(this.image, this.x, this.y);
+    }
+}
+
+const background = new GameObject('images/background.png');
+const hero=new GameObject('images/hero.png');
+const monster=new GameObject('images/monster.png');
+const canvas=document.createElement('canvas'); // создали элемент и задали параметры можно создать в HTML
+canvas.width=512;
+canvas.height=480;
+document.body.appendChild(canvas);
+hero.x=canvas.width/2;
+hero.y=canvas.height/2;
+
+monster.x=monster.image.width+(Math.random()*(canvas.width-monster.image.width*2));
+monster.y=monster.image.height+(Math.random()*(canvas.height-monster.image.height*2));
+
+//еще один вариант запуска события
+background.image.onload=()=>ctx.drawImage(background.image, 0, 0); //onload() запускается таким образом, когда
+//происходит загрузка события
+const ctx=canvas.getContext('2d') //(бываем 2d и 3d)// вывели canvas на экран
+window.addEventListener('keydown', (event)=>{
+    console.log(event);
+    if (event.key==="ArrouUp"){
+        hero.moveUp();
+    }
+    if (event.key==="ArrouUp"){
+        hero.moveDown();
+    }
+    if (event.key==="ArrouRight"){
+        hero.moveRight();
+    }
+    if (event.key==="ArrouLeft"){
+        hero.moveLeft();
+    }
+});
+
+window.addEventListener('keyup', (event)=>{
+    console.log(event);
+});
+
+const gameCycle=function (){
+    //console.log(gameCycle); // чтобы посмотреть
+    background.render(ctx);
+    hero.render(ctx);
+    monster.render(ctx);
+    window.requestAnimationFrame(gameCycle); //рекурсия
+}
+
+window.requestAnimationFrame(gameCycle);
+
+
+// setTimeout(()=>{
+//     background.render(ctx);
+//     hero.render(ctx);
+//     monster.render(ctx);
+// }, 1500);
+
+
