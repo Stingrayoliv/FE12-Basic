d2d8b1f9093d6937ae9efb3e22db0cb102c1892d
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2020-11-30 20:08:26 +0100

    Homework "Running away monster", with randoms. right version

--- game/monster.js
+++ game/monster.js
@@ -3,17 +3,39 @@ function createMonster(imageUrl, canvas) {
 
     Object.assign(monster, MovableGameObjectPrototype);
 
-    monster.reset = function() {
+    monster.direction = 0;
+    monster.movingTime = 0;
+
+    monster.reset = function () {
         this.x = this.image.width + Math.random() * (canvas.width - 3 * this.image.width);
         this.y = this.image.height + Math.random() * (canvas.height - 3 * this.image.height);
-        this.speed = 0.5;
     }
 
     monster.reset();
 
+
     monster.update = function (delta) {
-        this.moveRight(delta);
-        this.moveDown(delta);
+        if (this.movingTime <= 0) {
+            this.movingTime = Math.floor(Math.random() * 1000) + 100;
+            this.speed = Math.floor(Math.random() * 5 + 0.05);
+            this.direction = Math.round(Math.random() * 4 + 1)
+        }
+
+        switch (this.direction) {
+            case 0:
+                this.moveRight(delta);
+                break;
+            case 1:
+                this.moveLeft(delta);
+                break;
+            case 2:
+                this.moveUp(delta);
+                break;
+            case 3:
+                this.moveDown(delta);
+                break;
+        }
+        this.movingTime-=delta;
         /*
         1. Generate random number defines how much time monster will move.
         2. Generate random number defines a direction (up, down, left, right and their combinations)
