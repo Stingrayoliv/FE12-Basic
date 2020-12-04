12bbb81e83ca648fe20c713e98a7ce1f87578d2a
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2020-12-04 21:08:10 +0100

    finishing games cycle (part 1) time 30 sek and Space

--- game/score.js
+++ game/score.js
@@ -1,16 +1,23 @@
-function createScore() {
+function createScore(time) {
     const score = new GameObject();
+    const timeOfGame = time;
     score.x = 32;
     score.y = 32;
     score.wins = 0;
-    score.timeToEnd = 5 * 1000;// чтобы в мили сек было
+    score.timeToEnd = timeOfGame;
     score.isGameOver = false;
+
     score.update = function (delta) {
-        if (score.timeToEnd <= 0) {
+        if (this.timeToEnd <= 0) {
             this.isGameOver = true;
             this.timeToEnd = 0;
         } else {
-            score.timeToEnd -= delta;
+            this.timeToEnd -= delta;
+        }
+
+        if (keysPressed["Space"]){
+            this.isGameOver = false;
+            this.timeToEnd = timeOfGame;
         }
     }
 
@@ -19,9 +26,11 @@ function createScore() {
         ctx.font = "24px Helvetica";
         ctx.textAlign = "left";
         ctx.textBaseline = "top";
-        if(this.isGameOver){
-            ctx.fillText("The game is over! Your score: " + score.wins, 150, 200);
-        }else{
+        if (this.isGameOver) {
+            ctx.fillText("The game is over! Your score is: " + this.wins, this.x + 50, this.y + 150);
+            ctx.fillText("If you want to play again, press Space", this.x + 30, this.y + 200);
+        }
+        else {
             ctx.fillText("Monsters caught:" + this.wins, this.x, this.y);
             ctx.fillText("Seconds left: " + Math.floor(this.timeToEnd / 1000), this.x + 250, this.y);
         }
